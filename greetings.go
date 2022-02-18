68d64f9fe6f9d1937e225665467cc2a9fc9411b1
gyroflaw <ryo.kanazawa34@gmail.com>
2022-02-18 22:33:13 +0900

    Add Hellos

---
+++ greetings.go
@@ -0,0 +1,46 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Return Hello message
+func Hello(name string) (string,error) {
+	if(name==""){
+		return "",errors.New("empty name")
+	}
+	message := fmt.Sprintf(randomFormat(),name)
+	return message,nil
+}
+
+func Hellos(names []string) (map[string]string,error){
+
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message,err := Hello(name)
+		if(err!=nil){
+			return nil,err
+		}
+		messages[name]=message;
+	}
+	return messages,nil
+}
+
+func init(){
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi %v Welcome!",
+		"Greet to %v",
+		"Nict to meet %v",
+	}
+
+	return formats[rand.Intn(len(formats))]
+}
